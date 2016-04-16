f5116a0c3bdad96de30ba9504672b077ecaae6d5
JonathanDechelle <jonathan.dechelle@hotmail.com>
2016-04-16 11:27:35 -0400

    added miniStructure for the population first pass

---
+++ GeneticProgramming/GeneticProgramming/GeneticProgramming/GeneticAlgo/GeneticOperateur.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GeneticProgramming
+{
+    public class GeneticOperateur
+    {
+        /*enjambement : Possibilité de chance qu’un bit soit
+                        échangé avec son homologue sur l’autre chromosome */
+        public void CrossOver()
+        {
+
+        }
+
+        //mutation : Possibilité de chance q’un bit mute (passer de 1 à 0 ou de 0 à 1)
+        public void Mutate()
+        {
+
+        }
+    }
+}
