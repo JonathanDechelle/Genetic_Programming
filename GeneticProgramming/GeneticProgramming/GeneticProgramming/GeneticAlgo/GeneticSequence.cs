68b9ef0b81cbbcafca30277f776f650f8e4edfb2
JonathanDechelle <jonathan.dechelle@hotmail.com>
2016-05-01 08:46:10 -0400

    added Finish experience state and make example in genetic sequence pass 1

--- GeneticProgramming/GeneticProgramming/GeneticProgramming/GeneticAlgo/GeneticSequence.cs
+++ GeneticProgramming/GeneticProgramming/GeneticProgramming/GeneticAlgo/GeneticSequence.cs
@@ -15,13 +15,18 @@ namespace GeneticProgramming
         RANDOM_MUTATION = 5,
         REPLACE_OLD_GENERATION = 6,
         UPDATE_SITUATION = 7,
+        EXPERIENCE_FINISH = 8,
     }
 
     public class GeneticSequence
     {
         private StateMachine m_StateMachine;
+        private Situation m_Situation;
+        private SituationData m_SituationData;
+        private Population m_Population;
+        private int m_NbGenerations = 0;
 
-        public GeneticSequence()
+        public GeneticSequence(SituationData aSituationData)
         {
             m_StateMachine = new StateMachine();
             m_StateMachine.AddState(ESequenceState.GENERATE_FIRST_POPULATION, Status.OnEnter, OnEnterGenerateFirstPopulation);
@@ -36,6 +41,9 @@ namespace GeneticProgramming
             m_StateMachine.AddState(ESequenceState.REPLACE_OLD_GENERATION, Status.OnExit, OnExitReplaceOldGeneration);
             m_StateMachine.AddState(ESequenceState.UPDATE_SITUATION, Status.OnEnter, OnEnterUpdateSituation);
             m_StateMachine.AddState(ESequenceState.UPDATE_SITUATION, Status.OnExit, OnExitUpdateSituation);
+            m_StateMachine.AddState(ESequenceState.EXPERIENCE_FINISH, Status.OnEnter, OnEnterExperienceFinish);
+
+            m_SituationData = aSituationData;
         }
 
         public void Update()
@@ -45,17 +53,30 @@ namespace GeneticProgramming
 
         public void OnEnterGenerateFirstPopulation()
         {
+            m_Population = new Population(m_SituationData);
+            m_Population.GenerateAdditionalPopulation(m_SituationData);
 
+            m_Situation = new Situation(m_SituationData);
+            GeneticOperator.SetOperatorPercents(m_SituationData);
         }
 
         public void OnExitGenerateFirstPopulation()
+        {
+            m_Population.ComputeAdaptation();
+            m_SituationData.m_CurrentMaxAdaptation = m_Population.GetMaxAdaptation();
+        }
+
+        public void OnEnterExperienceFinish()
         {
 
         }
 
         public void OnEnterGenerateNewPopulation()
         {
-
+            if (m_Situation.HasSucceeded)
+            {
+                m_StateMachine.SetState(ESequenceState.EXPERIENCE_FINISH);
+            }
         }
 
         public void OnEnterGetTheElites()
